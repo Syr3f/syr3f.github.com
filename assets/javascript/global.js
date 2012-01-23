be1831d2cfaad40b1ae4977912eafa36fcdf0fba
Seraf Dos Santos <serafim@cyb3r.ca>
2012-01-22 22:13:56 -0500

    Initial js for showing dynamic repos

---
+++ assets/javascript/global.js
@@ -0,0 +1,33 @@
+
+var Displayer = Class.create({
+    
+    initialize: function (account) {
+        
+        this._account = account;
+    },
+    showRepos: function () {
+        
+        jQuery.ajax({
+            dataType: 'json',
+            error: function (jqXHR, txtStatus, errThrown) {
+                
+                alert("Error: "+txtStatus+"; "+errThrown);
+            },
+            success: function (data, txtStatus, jqXHR) {
+                
+                alert(data);
+            },
+            type: 'GET',
+            url: 'https://api.github.com/users/'+this._account+'/repos';
+        });
+    }
+});
+
+var _account = 'Syr3f';
+
+jQuery(document).ready(function ($) {
+    
+    var _displayer = new Displayer(_account);
+    
+    _displayer.showRepos();
+});
